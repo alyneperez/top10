e77c88fcdefc8f06b0ade0931ae1081917ed3426
alyneperez <alyne.kmp@gmail.com>
2022-08-17 19:39:22 -0300

    feat: get_movies view creation and ulrs patterns

--- Top10/movies/urls.py
+++ Top10/movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, get_movies
 
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path(" ", IndexView.as_view(), name="index"),
+    path("allmovies/", get_movies, name="get_movies")
 ]
--- Top10/movies/views.py
+++ Top10/movies/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.core.serializers import serialize
+from django.http import HttpResponse
+from .models import User, Movie, Like
 
 
 class IndexView(TemplateView):
     template_name = "movies/index.html"
 
-#TO DO:
-#fixtures dos filmes
-#views retornando json
+
+# TODO:
+# fixtures dos filmes
+# views retornando json
 # padrao url
+
+# https://dev.to/brian101co/how-to-return-a-json-response-in-django-gen
+
+
+def get_movies(request):
+    movies = Movie.objects.all()
+    data = serialize(
+        "json",
+        movies,
+        fields=("name", "original_name", "director", "genre", "year", "description"),
+    )
+    return HttpResponse(data, content_type="application/json")
+
+# http://127.0.0.1:8000/movies/allmovies/
